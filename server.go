7679c3ec144edfc1a8ee400c2a5aca44431738cb
szto <sztolabs@gmail.com>
2020-07-16 15:06:18 +0900

    feat: server 추가 및 router 분리 - 테스트 코드 작성

--- server.go
+++ server.go
@@ -2,14 +2,27 @@ package main
 
 import "fmt"
 import "github.com/gin-gonic/gin"
+import "net/http"
 
-func main() {
-	fmt.Println("vim-go")
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		fmt.Println(name)
+		// c.String(http.StatusOK, "Hello %s", name)
+		c.JSON(http.StatusOK, gin.H{"user": name})
+	})
+	return router
+
+}
+
+func main() {
+	router := setupRouter()
+	router.Run(":8080")
 }
