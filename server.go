6bdece889832077e3f747dbe8cf3b3571d3b0761
szto <sztolabs@gmail.com>
2020-07-16 15:20:24 +0900

    feat: test for html

--- server.go
+++ server.go
@@ -1,10 +1,29 @@
 package main
 
-import "fmt"
-import "github.com/gin-gonic/gin"
-import "net/http"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+var html = template.Must(template.New("http").Parse(`
+<html>
+	<head>
+		<title>http test</title>
+	</head>
+	<body>
+		<h1 style="color:red;"> Welcome.</h1>
+	</body>
+</html>
+`))
 
 func setupRouter() *gin.Engine {
+	logger := log.New(os.Stderr, "", 0)
+	logger.Println("DON'T USE THIS")
+
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -12,12 +31,22 @@ func setupRouter() *gin.Engine {
 		})
 	})
 
+	// return user name
 	router.GET("/user/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		fmt.Println(name)
 		// c.String(http.StatusOK, "Hello %s", name)
 		c.JSON(http.StatusOK, gin.H{"user": name})
 	})
+
+	// return html
+	router.SetHTMLTemplate(html)
+	router.GET("/welcome", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "http", gin.H{
+			"status": "success",
+		})
+	})
+
 	return router
 
 }
